1a3e329a14a78cb97367ce12b612578418ecfc8e
Otabek Kurbonov <otabekkurbonov1995@gmail.com>
2017-11-11 19:08:21 -0500

    fixed some formatting issues

--- HackerRank/src/leetcode/LongestUnivaluePath.java
+++ HackerRank/src/leetcode/LongestUnivaluePath.java
@@ -1,7 +1,5 @@
 package leetcode;
 
-import javax.swing.tree.TreeNode;
-
 /*687 Given a binary tree, find the length of the longest path where each node in the path has the same value.
 This path may or may not pass through the root. Note: The length of path between two nodes is represented by
 the number of edges between them. Input:
