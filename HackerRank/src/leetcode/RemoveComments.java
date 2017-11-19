141b89396e97807337a5e7b2318bbc20123afb67
Otabek Kurbonov <otabekkurbonov1995@gmail.com>
2017-11-18 21:24:54 -0500

    added a new solved algo from the leetcode on the new problem split linked list in parts

--- HackerRank/src/leetcode/RemoveComments.java
+++ HackerRank/src/leetcode/RemoveComments.java
@@ -6,7 +6,7 @@ import java.util.List;
 /*722 Given a C++ program, remove comments from it. The program source is an array where source[i] is the
 i-th line of the source code. This represents the result of splitting the original source code string by
 the newline character \n. In C++, there are two types of comments, line comments, and block comments. // and /*
-After removing the comments from the source code, return the source code in the same format*/     format fix
+After removing the comments from the source code, return the source code in the same format*/
 
 public class RemoveComments {
 	
