5b3856031f3f7adc358bcb21f37cbb037413585f
Beck Kurbonov <beck.kurbonov@ticketmaster.com>
2017-11-22 09:27:55 -0500

    added a new solved algo from the leetcode on new problem Baseball Game

--- HackerRank/src/tutorialFromWebsites/ArrayStream.java
+++ HackerRank/src/tutorialFromWebsites/ArrayStream.java
@@ -1,10 +1,6 @@
 package tutorialFromWebsites;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
-
-import static java.awt.Color.RED;
 
 /*All those primitive streams work just like regular object streams with the following differences:
 Primitive streams use specialized lambda expressions, e.g. IntFunction instead of Function or
@@ -16,9 +12,9 @@ public class ArrayStream {
 
         Stream.of("d2", "a2", "b1", "b3", "c")
                 .filter(s -> {
-                    System.out.println("filter: " + s);
+                    System.err.println("filter: " + s);
                     return true;
                 })
-                .forEach(s -> System.out.println("forEach: " + s));
+                .forEach(s -> System.err.println("forEach: " + s));
     }
 }
