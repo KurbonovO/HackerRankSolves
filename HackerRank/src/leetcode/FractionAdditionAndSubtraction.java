8f58cc9aa4daf2e69f37aab69b92a9cee68405a8
Beck Kurbonov <beck.kurbonov@ticketmaster.com>
2017-12-01 16:14:45 -0500

    added a new solved algo from leetcode on the new problem Fraction Addition and Subtraction

---
+++ HackerRank/src/leetcode/FractionAdditionAndSubtraction.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.Scanner;
+
+/*592 Given a string representing an expression of fraction addition and subtraction,
+you need to return the calculation result in string format. The final result should be
+irreducible fraction. If your final result is an integer, say 2, you need to change it
+to the format of fraction that has denominator 1. So in this case, 2 should be converted to 2/1.
+Input:"-1/2+1/2"
+Output: "0/1"
+Input:"-1/2+1/2+1/3"
+Output: "1/3"
+Input:"1/3-1/2"
+Output: "-1/6"
+Input:"5/3+1/3"
+Output: "2/1"*/
+
+public class FractionAdditionAndSubtraction {
+	public String fractionAddition(String expression) {
+		
+		Scanner sc = new Scanner(expression).useDelimiter("/|(?=[-+])");
+		
+		int A = 0, B = 1;
+		
+		while (sc.hasNext()) {
+			int a = sc.nextInt(), b = sc.nextInt();
+			A = A * b + a * B;
+			B *= b;
+			int g = gcd(A, B);
+			A /= g;
+			B /= g;
+		}
+		return A + "/" + B;
+	}
+
+	private int gcd(int a, int b) {
+		return a != 0 ? gcd(b % a, a) : Math.abs(b);
+	}
+}
