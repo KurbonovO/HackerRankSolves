e331297990ac9dcb83a0de3a5d32e1844be2d637
Otabek Kurbonov <otabekkurbonov1995@gmail.com>
2017-12-20 20:16:20 -0500

    added a new solved algo from the leet code on the medium the problem Sort Colors

---
+++ HackerRank/src/leetcode_Medium/SortColors.java
@@ -0,0 +1,36 @@
+package leetcode_Medium;
+
+/*75 Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent,
+with the colors in the order red, white and blue. Here, we will use the integers 0, 1, and 2 to represent the color red,
+white, and blue respectively		*/
+
+public class SortColors {
+	public void sortColors(int[] A) {
+		
+		if (A == null || A.length < 2)
+			return;
+		int low = 0;
+		int high = A.length - 1;
+		
+		for (int i = low; i <= high;) {
+			
+			if (A[i] == 0) {
+			  //swap A[i] and A[low] and i,low both ++
+				int temp = A[i];
+				A[i] = A[low];
+				A[low] = temp;
+				i++;
+				low++;
+			} 
+			else if (A[i] == 2) {
+			  //swap A[i] and A[high] and high--;
+				int temp = A[i];
+				A[i] = A[high];
+				A[high] = temp;
+				high--;
+			} else {
+				i++;
+			}
+		}
+	}
+}
