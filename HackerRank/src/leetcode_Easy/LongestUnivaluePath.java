279083f7e47835013275fc5f2f45554af2b1e2a0
Otabek Kurbonov <otabekkurbonov1995@gmail.com>
2017-12-31 18:13:31 -0500

    added a new solved algo from the leetcode on the problem Lowerst common ancestor of a binary search tree

--- HackerRank/src/leetcode_Easy/LongestUnivaluePath.java
+++ HackerRank/src/leetcode_Easy/LongestUnivaluePath.java
@@ -1,5 +1,7 @@
 package leetcode_Easy;
 
+import util.TreeNode;
+
 /*687 Given a binary tree, find the length of the longest path where each node in the path has the same value.
 This path may or may not pass through the root. Note: The length of path between two nodes is represented by
 the number of edges between them. Input:
@@ -17,13 +19,6 @@ Input:
           4   4   5  Output: 2
 Longest-Univalue-Path of a tree is among those Longest-Univalue-Path-Across at each node;
 Longest-Univalue-Path-Across a node is sum of { Longest-Univalue-Path-Start-At each child with same value, + 1}*/
-
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-	TreeNode(int x) {val = x;}
-}
 
 public class LongestUnivaluePath {
 
--- HackerRank/src/leetcode_Easy/LowestCommonAncestorOfABinarySearchTree.java
+++ HackerRank/src/leetcode_Easy/LowestCommonAncestorOfABinarySearchTree.java
@@ -1,5 +1,61 @@
 package leetcode_Easy;
 
-public class LowestCommonAncestorOfABinarySearchTree {
+import util.TreeNode;
 
+/*117 Follow up for problem "Populating Next Right Pointers in Each Node". What
+if the given tree could be any binary tree? Would your previous solution still 
+work? You may only use constant extra space.
+Given the following binary tree,
+         1
+       /  \
+      2    3
+     / \    \
+    4   5    7
+After calling your function, the tree should look like:
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \    \
+    4-> 5 -> 7 -> NULL														  */
+
+public class LowestCommonAncestorOfABinarySearchTree {
+	public void connect(TreeNode treeNode) {
+        
+	  //The left most node in the lower level
+		TreeNode head = treeNode;
+	  //The previous node in the lower level
+		TreeNode prev = null;
+	  //The current node in the upper level
+		TreeNode curr = null;
+        
+		while (head != null){
+            curr = head;
+            prev = null;
+            head = null;
+            
+            while (curr != null){
+                
+            	if (curr.left != null){
+                    
+            		if (prev != null)
+                        prev.next=curr.left;
+                    
+            		else 
+                        head = curr.left;
+                    prev = curr.left;
+                }
+                
+            	if (curr.right != null){
+                    
+            		if (prev != null)
+                        prev.next = curr.right;
+                    
+            		else 
+                        head = curr.right;
+                    prev = curr.right;
+                }
+                curr = curr.next;
+            }
+        }
+    }
 }
