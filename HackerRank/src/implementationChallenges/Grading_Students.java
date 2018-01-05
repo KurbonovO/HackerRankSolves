6364f5c65037e6688c206440bc5d034cd028798d
Otabek Kurbonov <otabekkurbonov1995@gmail.com>
2018-01-04 21:56:09 -0500

    added a new solved algo from the leetcode on the new problem Longest repeating character replacement

--- HackerRank/src/implementationChallenges/Grading_Students.java
+++ HackerRank/src/implementationChallenges/Grading_Students.java
@@ -1,14 +1,12 @@
 package implementationChallenges;
 import java.util.Scanner;
 
-/*
- * Every student receives a grade in the inclusive range from 0 to 100.
- * Any grade less than 40 is a failing grade.
- * If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
- * For example, grade 84 will be rounded to 85 but grade 29 will not be rounded because the rounding would result in a number that is less than 40.
- * Sample Input:  4 73 67 38 33
- * Sample Output:   75 67 40 33
- */
+/*Every student receives a grade in the inclusive range from 0 to 100. Any grade
+less than 40 is a failing grade. If the difference between the grade and the next
+multiple of 5 is less than 3, round grade up to the next multiple of 5. For example,
+grade 84 will be rounded to 85 but grade 29 will not be rounded because the rounding
+would result in a number that is less than 40.
+Sample Input:  4 73 67 38 33					 Sample Output:   75 67 40 33 */
 
 public class Grading_Students {
 	
