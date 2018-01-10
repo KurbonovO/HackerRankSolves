948c1e13475d4ea928a7e471fd4937ae1af42ed6
Otabek Kurbonov <otabekkurbonov1995@gmail.com>
2018-01-09 22:29:00 -0500

    added a new solved algo from the hackerrank on the 30 day challanage Day 3

--- HackerRank/src/hacker_rank_Thirty/DayThree.java
+++ HackerRank/src/hacker_rank_Thirty/DayThree.java
@@ -2,42 +2,29 @@ package hacker_rank_Thirty;
 
 import java.util.Scanner;
 
-/*Given the meal price (base cost of a meal), tip percent (the percentage of the
-meal price being added as tip), and tax percent (the percentage of the meal price
-being added as tax) for a meal, find and print the meal's total cost          */
-
 public class DayThree {
+	private static String weird = "Weird";
+	private static String notWeird = "Not Weird";
 	public static void main(String[] args) {
-
 		Scanner scan = new Scanner(System.in);
-
-		// original meal price
-		double mealCost = scan.nextDouble();
-		// tip percentage
-		int tipPercent = scan.nextInt();
-		// tax percentage
-		int taxPercent = scan.nextInt();
-
+		int n = scan.nextInt();
 		scan.close();
+		String ans = "";
 
-		// Write your calculation code here.
-		double tip = mealCost * tipPercent / 100;
-		double tax = mealCost * taxPercent / 100;
-
-		// cast the result of the rounding operation to an int and save it as totalCost
-		int totalCost = (int) Math.round(tip + tax);
-
-		// Print your result
-		System.out.println(totalCost);
+		// if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+		if (n % 2 == 1) {
+			ans = weird;
+		}
+		// 'n' must be even; proceed if 'n' >= 2
+		else if (n >= 2) {
+			if (n > 20) {
+				ans = notWeird;
+			} else if (n >= 6) {
+				ans = weird;
+			} else if (n >= 2) {
+				ans = notWeird;
+			}
+		}
+		System.out.println(ans);
 	}
 }
-
-/*Sample Input: 
-12.00
-20
-8
-Sample Output: The total meal cost is 15 dollars.
-Explanation: Given: mealCost = 12, tipPercent = 20, taxPercent = 8 Calculations: 
-tip = 12 * (20/100) = 2.4 
-tax = 12 * (8/100)  = .96 We round  to the nearest dollar (integer) and then 
-print our result															  */
