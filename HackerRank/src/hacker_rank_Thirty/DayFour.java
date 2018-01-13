9f58187bc68513c3c65415455d45f319d1012a71
Otabek Kurbonov <otabekkurbonov1995@gmail.com>
2018-01-12 22:38:58 -0500

    added a new day on the 30 day challange

--- HackerRank/src/hacker_rank_Thirty/DayFour.java
+++ HackerRank/src/hacker_rank_Thirty/DayFour.java
@@ -1,5 +1,10 @@
 package hacker_rank_Thirty;
 
+/*we're going to learn about the difference between a class and an instance; because
+this is an Object Oriented concept, it's only enabled in certain languages. 
+ * 
+ */
+
 public class DayFour {
 	
 	private int age;
