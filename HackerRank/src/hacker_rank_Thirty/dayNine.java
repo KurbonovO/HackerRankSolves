b940b3aaba5400da8d7aac51426cee52a068bfff
Otabek Kurbonov <otabekkurbonov1995@gmail.com>
2018-01-16 23:01:03 -0500

    added day 9 of 30

---
+++ HackerRank/src/hacker_rank_Thirty/dayNine.java
@@ -0,0 +1,18 @@
+package hacker_rank_Thirty;
+
+import java.util.Scanner;
+
+public class dayNine {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		in.close();
+		System.out.println(factorial(n));
+	}
+
+	private static int factorial(int n) {
+		if (n == 1)
+			return 1;
+		return factorial(n - 1) * n;
+	}
+}
