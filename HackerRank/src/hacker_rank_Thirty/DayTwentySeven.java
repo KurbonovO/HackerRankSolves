31f8ba4f651da2ce4e2c7e03f28c089e53350b1e
Otabek Kurbonov <otabekkurbonov1995@gmail.com>
2018-02-16 21:09:27 -0500

    added a day 27 out of the day 30 on hacker rank

---
+++ HackerRank/src/hacker_rank_Thirty/DayTwentySeven.java
@@ -0,0 +1,23 @@
+package hacker_rank_Thirty;
+
+/*generate a valid test case for a problem instead of solving the problem. There
+is no input to read, you simply have to generate and print test . 
+ * 
+ */
+
+public class DayTwentySeven {
+	public static void main(String srgs[]) {
+		
+		System.out.println(5);
+		System.out.println("4 3");
+		System.out.println("0 -3 4 2");
+		System.out.println("5 2");
+		System.out.println("0 -3 4 2 2");
+		System.out.println("3 3");
+		System.out.println("0 -3 4");
+		System.out.println("7 2");
+		System.out.println("0 -3 1 1 1 1 1");
+		System.out.println("6 3");
+		System.out.println("0 -3 4 2 1 1");
+	}
+}
