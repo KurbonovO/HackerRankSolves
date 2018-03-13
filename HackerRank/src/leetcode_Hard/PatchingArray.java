cd6b7beba1487a608dcdb15f960c8a590f1df096
Otabek Kurbonov <otabekkurbonov1995@gmail.com>
2018-03-12 21:51:19 -0400

    format fix

--- HackerRank/src/leetcode_Hard/PatchingArray.java
+++ HackerRank/src/leetcode_Hard/PatchingArray.java
@@ -6,7 +6,23 @@ by the sum of some elements in the array. Return the minimum number of patches
 required																	  */
 
 public class PatchingArray {
+	public int minPatches(int[] nums, int n) {
+		
+		long missing = 1;
+		int patches = 0, i = 0;
 
+		while (missing <= n) {
+			
+			if (i < nums.length && nums[i] <= missing) {
+				missing += nums[i++];
+			} 
+			else {
+				missing += missing;
+				patches++;
+			}
+		}
+		return patches;
+	}
 }
 
 /*nums = [1, 3], n = 6
