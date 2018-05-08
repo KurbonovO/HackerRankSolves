114e3902346edc03d3b48bae0628cdb6192b9cef
Otabek Kurbonov <otabekkurbonov1995@gmail.com>
2018-05-07 22:47:27 -0400

    format

--- src/main/java/leetcode_Easy/ContainsDuplicate.java
+++ src/main/java/leetcode_Easy/ContainsDuplicate.java
@@ -1,7 +1,8 @@
 package leetcode_Easy;
 
-/*217 Given an array of integers, find if the array contains any duplicates. Your function should return true
-if any value appears at least twice in the array, and it should return false if every element is distinct*/
+/*217 Given an array of integers, find if the array contains any duplicates.
+Your function should return true if any value appears at least twice in the
+array, and it should return false if every element is distinct			     */
 
 public class ContainsDuplicate {
 	public boolean ContainsDuplicateMethod(int[] nums) {
