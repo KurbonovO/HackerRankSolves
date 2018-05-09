629356faf3c316d14b8d34c3109c79948dee67ec
Otabek Kurbonov <otabekkurbonov1995@gmail.com>
2018-05-08 23:05:25 -0400

    Formatted more classes

--- src/main/java/leetcode_Easy/BaseballGame.java
+++ src/main/java/leetcode_Easy/BaseballGame.java
@@ -2,19 +2,21 @@ package leetcode_Easy;
 
 import java.util.Stack;
 
-/*682 You're now a baseball game point recorder. Given a list of strings, each string can be one of
-the 4 following types:
-1) Integer (one round's score): Directly represents the number of points you get in this round.
-2) "+" (one round's score): Represents that the points you get in this round are the sum of the last two
-valid round's points.
-3) "D" (one round's score): Represents that the points you get in this round are the doubled data of the
-last valid round's points.
-4) "C" (an operation, which isn't a round's score): Represents the last valid round's points you get were
-invalid and should be removed.
-Each round's operation is permanent and could have an impact on the round before and the round after.
-You need to return the sum of the points you could get in all the rounds
-*!*Let's maintain the value of each valid round on a stack as we process the data. A stack is ideal since we only
-deal with operations involving the last or second-last valid round.*/
+/*682 You're now a baseball game point recorder. Given a list of strings, each
+string can be one of the 4 following types:
+1) Integer (one round's score): Directly represents the number of points you get
+in this round.
+2) "+" (one round's score): Represents that the points you get in this round are
+the sum of the last two valid round's points.
+3) "D" (one round's score): Represents that the points you get in this round are
+the doubled data of the last valid round's points.
+4) "C" (an operation, which isn't a round's score): Represents the last valid
+round's points you get were invalid and should be removed.
+Each round's operation is permanent and could have an impact on the round before
+and the round after. You need to return the sum of the points you could get in
+all the rounds *!*Let's maintain the value of each valid round on a stack as we
+process the data. A stack is ideal since we only deal with operations involving
+the last or second-last valid round											 */
 
 public class BaseballGame {
 	public int calPoints(String[] ops) {
@@ -67,4 +69,4 @@ Operation 1: The round 3's data is invalid. The sum is: 3.
 Round 4: You could get -4 points (the round 3's data has been removed). The sum is: -1.
 Round 5: You could get 9 points. The sum is: 8.
 Round 6: You could get -4 + 9 = 5 points. The sum is 13.
-Round 7: You could get 9 + 5 = 14 points. The sum is 27.*/
+Round 7: You could get 9 + 5 = 14 points. The sum is 27						 */
