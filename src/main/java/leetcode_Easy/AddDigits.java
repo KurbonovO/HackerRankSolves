f94f8d0fcb047d9941f40662836f20081231ce2f
Otabek Kurbonov <otabekkurbonov1995@gmail.com>
2018-08-15 08:14:30 -0400

    small format changes going to the master branch.

--- src/main/java/leetcode_Easy/AddDigits.java
+++ src/main/java/leetcode_Easy/AddDigits.java
@@ -3,8 +3,8 @@ package leetcode_Easy;
 /*258 Given a non-negative integer num, repeatedly add all its digits until the
 result has only one digit.
 Input: 38			Output: 2 
-The process is like: 3 + 8 = 11, 1 + 1 = 2 Since 2 has only one digit, 
-return it */
+The process is like: 3 + 8 = 11, 1 + 1 = 2 Since 2 has only one digit, return
+it */
 
 public class AddDigits {
 	public int addDigits(int intNInput) {
