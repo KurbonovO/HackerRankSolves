84d33ae89e4665a50672c9312df23c7ce4db72a4
Otabek Kurbonov <Otabek.Kurbonov@cns-inc.com>
2019-01-15 16:54:09 -0500

    Removing the east answers from the repo once again.

--- src/main/java/leetcode_Easy/JewelsAndStonesLeetCode.java
+++
@@ -1,8 +0,0 @@
-package leetcode_Easy;
-
-public class JewelsAndStonesLeetCode {
-	public int jewwlsAndStones(String jewels, String strones) {
-	
-		return strones.replaceAll("[^" + jewels + "]", "").length();
-	}
-}
