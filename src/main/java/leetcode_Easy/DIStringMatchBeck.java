14c40ba4987ec7544c78ff2d883d18dca3785f2d
Otabek Kurbonov <Otabek.Kurbonov@cns-inc.com>
2019-01-16 16:37:52 -0500

    Commit before leaving.

---
+++ src/main/java/leetcode_Easy/DIStringMatchBeck.java
@@ -0,0 +1,24 @@
+package leetcode_Easy;
+
+public class DIStringMatchBeck {
+	public int[] diStringMatchBeck(String input) {
+		
+		int lenghtOfInput = input.length();
+		
+		int[] outputArray = new int[lenghtOfInput];
+		
+		for(char c : input.toCharArray()) {
+			
+			if(c == 'I') {
+				
+				
+				
+			} else if(c == 'D') {
+				
+				
+				
+			}
+		}
+		return outputArray;
+	}
+}
