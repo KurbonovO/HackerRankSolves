9363965497e22b1ee65b4d2da3adecc976a51b33
Otabek Kurbonov <Otabek.Kurbonov@cns-inc.com>
2019-01-16 12:59:35 -0500

    Removing the not needed files.

--- src/main/java/leetcode_Easy/BinaryNumberWithAlternatingBits.java
+++
@@ -1,8 +0,0 @@
-package leetcode_Easy;
-
-public class BinaryNumberWithAlternatingBits {
-	public boolean hasAlternatingBits(int n) {
-
-		return Integer.toBinaryString(n).matches("(10)*1?");
-	}
-}
