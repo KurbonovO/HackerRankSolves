71466007c06cfe40138a9639a2f28007aebc24bf
Otabek Kurbonov <Otabek.Kurbonov@cns-inc.com>
2019-02-11 15:58:01 -0500

    Was not able to solve this algo but I think i got the concept of how doubling the string can use the contains

---
+++ src/main/java/crackingTheCodingInterview/StringRotationOne.java
@@ -0,0 +1,16 @@
+package crackingTheCodingInterview;
+
+public class StringRotationOne {
+	public boolean stringRotationOne(String s1, String s2) {
+
+		int s1lenght = s1.length();
+
+		/* Check that s1 and s2 are equal in length and not empty */
+		if (s1lenght == s2.length() && s1lenght > 0) {
+			/* Concatenate s1 and s2 within new buffer */
+			String s1s1 = s1 + s1;
+			return isSubstring(s1s1, s2);
+		}
+		return false;
+	}
+}
