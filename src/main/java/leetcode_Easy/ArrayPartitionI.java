58aef56464205d08e05ad8e5cdcecc2855a7c951
Beck Kurbonov <otabekkurbonov1995@gmail.com>
2019-03-18 21:52:57 -0400

    Solved a new algo from a leetcode on a new problem Array partiton I

---
+++ src/main/java/leetcode_Easy/ArrayPartitionI.java
@@ -0,0 +1,14 @@
+package leetcode_Easy;
+
+import java.util.Arrays;
+
+public class ArrayPartitionI {
+	public int arrayPairSum(int[] nums) {
+		Arrays.sort(nums);
+		int result = 0;
+		for (int i = 0; i < nums.length; i += 2) {
+			result += nums[i];
+		}
+		return result;
+	}
+}
