d3c327c9b1ad58ee7ec6bba30817883aab5dc3f0
Beck Kurbonov <otabekkurbonov1995@gmail.com>
2019-06-29 20:26:37 -0400

    Solved a new algo from a leetcode on a new problem Diameter of Binary Tree on a easy difficulty.

---
+++ src/main/java/leetcodeEasy/DiameterOfBinaryTreeLeetCode.java
@@ -0,0 +1,26 @@
+package leetcodeEasy;
+
+import util.TreeNode;
+
+public class DiameterOfBinaryTreeLeetCode {
+
+	int max = 0;
+
+	public int diameterOfBinaryTree(TreeNode root) {
+		maxDepth(root);
+		return max;
+	}
+
+	private int maxDepth(TreeNode root) {
+		if (root == null)
+			return 0;
+
+		int left = maxDepth(root.left);
+		int right = maxDepth(root.right);
+
+		max = Math.max(max, left + right);
+
+		return Math.max(left, right) + 1;
+	}
+
+}
