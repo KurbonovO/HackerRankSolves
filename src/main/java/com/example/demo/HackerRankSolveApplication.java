b9dae4c42affd61c41d586ff3d8040833ca41486
Beck Kurbonov <Otabek.Kurbonov@cns-inc.com>
2019-07-08 16:06:38 -0400

    added back the main class

---
+++ src/main/java/com/example/demo/HackerRankSolveApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerRankSolveApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(HackerRankSolveApplication.class, args);
+	}
+}
