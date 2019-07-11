1ec96d06e7d4bcd78975c2057044901380bf8e74
Beck Kurbonov <Otabek.Kurbonov@cns-inc.com>
2019-07-11 16:50:36 -0400

    Added one more String reversal solve.

--- src/main/java/leetcodeEasy/ReverseStringBeck.java
+++ src/main/java/leetcodeEasy/ReverseStringBeck.java
@@ -29,15 +29,31 @@ public class ReverseStringBeck {
 		return s.toString();
 	}
 
+	public static String reverseStringThree(String str) {
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = str.length() - 1; i >= 0; i--) {
+
+			sb.append(str.charAt(i));
+		}
+		return sb.toString();
+
+	}
+
 	public static void main(String[] args) {
 
 		char[] inputOne = { 'h', 'e', 'l', 'l', 'o' };
 
-		System.out.println("First One " + ReverseStringBeck.reverseStringOne(inputOne));
+		System.out.println("First " + ReverseStringBeck.reverseStringOne(inputOne));
 
 		char[] inputTwo = { 'H', 'a', 'n', 'n', 'a', 'h' };
 
-		System.out.println("Second One " + ReverseStringBeck.reverseStringTwo(inputTwo));
+		System.out.println("Second " + ReverseStringBeck.reverseStringTwo(inputTwo));
+
+		String inputThree = "Hello";
+
+		System.out.println("third " + ReverseStringBeck.reverseStringThree(inputThree));
 	}
 
 }
