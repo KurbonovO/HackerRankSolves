1f7c258549338b08d67c5ade7bce5650bcd1023c
Beck Kurbonov <otabekkurbonov1995@gmail.com>
2019-07-28 21:00:47 -0400

    Solved a new algo.

---
+++ src/main/java/leetcodeMedium/NumberOfMatchingSubsequencesLeetCode.java
@@ -0,0 +1,24 @@
+package leetcodeMedium;
+
+import java.text.StringCharacterIterator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberOfMatchingSubsequencesLeetCode {
+
+	public int numMatchingSubseq(String S, String[] words) {
+		List<StringCharacterIterator>[] waiting = new List[128];
+		for (int c = 0; c <= 'z'; c++)
+			waiting[c] = new ArrayList();
+		for (String w : words)
+			waiting[w.charAt(0)].add(new StringCharacterIterator(w));
+		for (char c : S.toCharArray()) {
+			List<StringCharacterIterator> advance = waiting[c];
+			waiting[c] = new ArrayList();
+			for (StringCharacterIterator it : advance)
+				waiting[it.next() % it.DONE].add(it);
+		}
+		return waiting[0].size();
+	}
+
+}
