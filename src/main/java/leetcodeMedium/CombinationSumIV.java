455e1573aad0d5e9d393bdeb13733e77ab007775
Beck Kurbonov <otabekkurbonov1995@gmail.com>
2019-12-16 21:01:22 -0500

    December 16 solves.

---
+++ src/main/java/leetcodeMedium/CombinationSumIV.java
@@ -0,0 +1,18 @@
+package leetcodeMedium;
+
+public class CombinationSumIV {
+
+	public int combinationSum4(int[] nums, int target) {
+		if (target == 0) {
+			return 1;
+		}
+		int res = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (target >= nums[i]) {
+				res += combinationSum4(nums, target - nums[i]);
+			}
+		}
+		return res;
+	}
+
+}
