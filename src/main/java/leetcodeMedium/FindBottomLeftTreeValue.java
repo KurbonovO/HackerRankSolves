3fd815a890aa0e7a519cca89b8ad748751cdf708
Beck Kurbonov <otabekkurbonov1995@gmail.com>
2020-01-05 20:03:00 -0500

    January 5 solves.

---
+++ src/main/java/leetcodeMedium/FindBottomLeftTreeValue.java
@@ -0,0 +1,23 @@
+package leetcodeMedium;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import util.TreeNode;
+
+public class FindBottomLeftTreeValue {
+
+	public int findLeftMostNode(TreeNode root) {
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(root);
+		while (!queue.isEmpty()) {
+			root = queue.poll();
+			if (root.right != null)
+				queue.add(root.right);
+			if (root.left != null)
+				queue.add(root.left);
+		}
+		return root.root;
+	}
+
+}
