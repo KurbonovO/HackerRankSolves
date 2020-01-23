c560cef7959251282e13be8f09aa7ddae620bebf
Beck Kurbonov <otabekkurbonov1995@gmail.com>
2020-01-22 23:23:07 -0500

    Jan 22 solves.

---
+++ src/main/java/leetcodeMedium/CheckIfWordIsValidAfterSubstitutionsD.java
@@ -0,0 +1,22 @@
+package leetcodeMedium;
+
+import java.util.Stack;
+
+public class CheckIfWordIsValidAfterSubstitutionsD {
+
+	public boolean isValid(String s) {
+		Stack<Character> stack = new Stack<>();
+		for (char c : s.toCharArray()) {
+			if (c == 'c') {
+				if (stack.isEmpty() || stack.pop() != 'b')
+					return false;
+				if (stack.isEmpty() || stack.pop() != 'a')
+					return false;
+			} else {
+				stack.push(c);
+			}
+		}
+		return stack.isEmpty();
+	}
+
+}
