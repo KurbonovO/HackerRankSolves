7ab93c85254c6e2cf56f2477b682fd470baa3ced
Beck Kurbonov <BeckKurbonov@gmail.com>
2020-05-20 23:07:16 -0400

    May 20 solves.

---
+++ src/main/java/leetcodeEasy/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,34 @@
+package leetcodeEasy;
+
+public class BestTimeToBuyAndSellStock {
+	
+	// Time Complexity: O(n)
+	// Space Complexity: O(1)
+
+	public int bestTimeToBuyAndSellStock(int[] prices) {
+
+		int minPrice = Integer.MAX_VALUE;
+		int maxProfit = 0;
+
+		for (int i = 0; i < prices.length; i++) {
+
+			if (prices[i] < minPrice) {
+
+				minPrice = prices[i];
+
+			} else if (prices[i] - minPrice > maxProfit) {
+
+				maxProfit = prices[i] - minPrice;
+			}
+		}
+		return maxProfit;
+	}
+
+	public static void main(String[] args) {
+
+		BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
+
+		System.out.println(bestTimeToBuyAndSellStock.bestTimeToBuyAndSellStock(new int[] { 7, 1, 5, 3, 6, 4 }));
+	}
+
+}
