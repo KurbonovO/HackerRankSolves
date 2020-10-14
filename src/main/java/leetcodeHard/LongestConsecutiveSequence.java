6bb2d7a81407116f024f37f3bf82c774c683b54e
Beck Kurbonov <BeckKurbonov@gmail.com>
2020-10-13 23:51:06 -0400

    minor fix

--- src/main/java/leetcodeHard/LongestConsecutiveSequence.java
+++ src/main/java/leetcodeHard/LongestConsecutiveSequence.java
@@ -52,4 +52,13 @@ public class LongestConsecutiveSequence {
 		return result;
 	}
 
+	public static void main(String[] args) {
+
+		LongestConsecutiveSequence longestConsecutiveSequence = new LongestConsecutiveSequence();
+
+		int[] array = new int[] { 100, 4, 200, 1, 3, 2 };
+
+		System.out.println(longestConsecutiveSequence.longestConsecutiveSequence(array));
+	}
+
 }
