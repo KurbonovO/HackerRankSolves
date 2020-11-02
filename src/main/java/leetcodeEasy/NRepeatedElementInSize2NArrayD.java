7ff729c03ccd06e870f3f06b6b3ad219626e68a2
Beck Kurbonov <BeckKurbonov@gmail.com>
2020-11-01 23:07:08 -0500

    November 1 solves.

---
+++ src/main/java/leetcodeEasy/NRepeatedElementInSize2NArrayD.java
@@ -0,0 +1,13 @@
+package leetcodeEasy;
+
+public class NRepeatedElementInSize2NArrayD {
+
+	public int repeatedNTimes(int[] A) {
+		int[] count = new int[10000];
+		for (int a : A)
+			if (count[a]++ == 1)
+				return a;
+		return -1;
+	}
+
+}
