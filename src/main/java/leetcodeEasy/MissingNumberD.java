1cb9cebfc0ab62ea70466902043316c83db6765b
Beck Kurbonov <BeckKurbonov@gmail.com>
2020-11-08 23:54:08 -0500

    November 8 solves.

---
+++ src/main/java/leetcodeEasy/MissingNumberD.java
@@ -0,0 +1,15 @@
+package leetcodeEasy;
+
+public class MissingNumberD {
+
+	public int missingNumber(int[] nums) {
+
+		int xor = 0, i = 0;
+		for (i = 0; i < nums.length; i++) {
+			xor = xor ^ i ^ nums[i];
+		}
+
+		return xor ^ i;
+	}
+
+}
