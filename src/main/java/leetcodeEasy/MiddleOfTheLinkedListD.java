05810efa78ff38ba76e71b4e4ef65f3cd91a95aa
Beck Kurbonov <BeckKurbonov@gmail.com>
2020-11-26 23:10:34 -0500

    November 26 solves.

---
+++ src/main/java/leetcodeEasy/MiddleOfTheLinkedListD.java
@@ -0,0 +1,16 @@
+package leetcodeEasy;
+
+import util.ListNode;
+
+public class MiddleOfTheLinkedListD {
+
+	public ListNode middleNode(ListNode head) {
+		ListNode slow = head, fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+
+}
