0c688f2e6f9f8b0749523be0280a0243893c2cfe
Beck Kurbonov <BeckKurbonov@gmail.com>
2020-12-05 21:23:09 -0500

    December 5 solves.

---
+++ src/main/java/leetcodeEasy/FindNUniqueIntegersSumUpToZeroD.java
@@ -0,0 +1,12 @@
+package leetcodeEasy;
+
+public class FindNUniqueIntegersSumUpToZeroD {
+
+	public int[] sumZero(int n) {
+		int[] A = new int[n];
+		for (int i = 0; i < n; ++i)
+			A[i] = i * 2 - n + 1;
+		return A;
+	}
+
+}
