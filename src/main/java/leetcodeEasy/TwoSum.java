85b6085933b27bf7bd8184bc0848239fd2200703
Beck Kurbonov <BeckKurbonov@gmail.com>
2021-01-09 23:00:52 -0500

    Fix for the two sum problem.

--- src/main/java/leetcodeEasy/TwoSum.java
+++ src/main/java/leetcodeEasy/TwoSum.java
@@ -25,9 +25,10 @@ public class TwoSum {
 	}
 
 	/**
-	 * TODO!!!!!! 
-	 * Time Complexity: O(n) 
-	 * Space Complexity: O(n)
+	 * Time Complexity: O(n). We traverse the list containing n elements only once. 
+	 * Each look up in the table costs only O(1) time.
+	 * Space Complexity: O(n). The extra space required depends on the number of items
+	 * stored in the hash table, which stores at most n elements.
 	 */
 	public static void main(String[] args) {
 
